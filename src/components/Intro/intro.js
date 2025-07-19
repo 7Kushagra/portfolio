0f5728249e9a84fa92d7b29aae3a2f9d34c0993b
Kushagra Pareek <kushagrapareek@Kushagras-MacBook-Air.local>
2025-07-20 02:14:58 +0530

    Added resume.pdf and updated Intro.jsx for download button

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './intro.css';
 import bg from '../../assets/image.png';
 import btnImg from '../../assets/hireme.png';
-import { Link } from 'react-scroll';
+
 
 const Intro = () => {
     return (
@@ -11,7 +11,12 @@ const Intro = () => {
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm <span className="introName">Kushagra</span> <br />Website Designer</span>
                 <p className="introPara">A Computer Science student passionate about web development, <br />problem-solving, and building smart. Always learning. Always creating.</p>
-                <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg'/> Download CV</button></Link>
+                <a href="/resume.pdf" download>
+  <button className="btn">
+    <img src={btnImg} alt="Download CV" className="btnImg" /> Download CV
+  </button>
+</a>
+
             </div>
             <img src={bg} alt="Profile" className="bg" />
         </section>
